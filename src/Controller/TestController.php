20e3fb92066e74a422cc3b1db1abbaeff2f113b9
kamga98 <smkamga@gmail.com>
2026-04-15 19:59:37 +0200

    Nouvelle sauvegarde integrale - 19h59

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 
 final class TestController extends AbstractController
@@ -15,21 +16,24 @@ final class TestController extends AbstractController
     public function index(): Response
     {  
    
-       $album = [
-            'artist' => 'Global Star',
-            'title' => 'Rise of a Legend',
-            'year' => 2026,
-            'cover' => 'https://via.placeholder.com/500',
-            'tracks' => [
-                ['name' => 'Intro', 'duration' => '1:12'],
-                ['name' => 'The Journey', 'duration' => '3:45'],
-                ['name' => 'Success Story', 'duration' => '4:20'],
-                ['name' => 'World Stage', 'duration' => '3:58'],
-                ['name' => 'Legacy', 'duration' => '5:10'],
-            ]
-        ];
-  
-        return $this->render('album/show.html.twig', $album);
+    //    $album = [
+    //         'artist' => 'Global Star',
+    //         'title' => 'Rise of a Legend',
+    //         'year' => 2026,
+    //         'cover' => 'https://via.placeholder.com/500',
+    //         'tracks' => [
+    //             ['name' => 'Intro', 'duration' => '1:12'],
+    //             ['name' => 'The Journey', 'duration' => '3:45'],
+    //             ['name' => 'Success Story', 'duration' => '4:20'],
+    //             ['name' => 'World Stage', 'duration' => '3:58'],
+    //             ['name' => 'Legacy', 'duration' => '5:10'],
+    //         ]
+    //     ];
+   
+       // return $this->render('album/show.html.twig', $album);
+
+        return $this->render('album/show.html.twig');
+
 
         // return $this->json([   
         //     'message' => 'Welcome to your new controller!',
