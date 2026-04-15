db7928349cc1a39a7105dbc094b114cc6e2e2c7c
kamga98 <smkamga@gmail.com>
2026-04-15 19:40:45 +0200

    Sauvegarde integrale

---
+++ src/Controller/TestController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+
+final class TestController extends AbstractController
+{
+   
+     /**    
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {  
+ 
+       // dd("Welcome to my house!!!!!!!!!!!!!!!!");   
+ 
+       $album = [
+            'artist' => 'Global Star',
+            'title' => 'Rise of a Legend',
+            'year' => 2026,
+            'cover' => 'https://via.placeholder.com/500',
+            'tracks' => [
+                ['name' => 'Intro', 'duration' => '1:12'],
+                ['name' => 'The Journey', 'duration' => '3:45'],
+                ['name' => 'Success Story', 'duration' => '4:20'],
+                ['name' => 'World Stage', 'duration' => '3:58'],
+                ['name' => 'Legacy', 'duration' => '5:10'],
+            ]
+        ];
+  
+        return $this->render('album/show.html.twig', $album);
+
+        // return $this->json([   
+        //     'message' => 'Welcome to your new controller!',
+        //     'path' => 'src/Controller/TestController.php',
+        // ]);
+    }
+}  
